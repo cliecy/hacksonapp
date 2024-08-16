12ba971f6199591f92b11a066d7353ce65c9b1c4
cliecy <guanjiezou@gmail.com>
2024-08-16 11:09:29 +0900

    this is some simple change to add the react-router to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import Testpage from './Pages/Testpage';
+// import Testpage from './Pages/Testpage';
+import Dashboard from './Pages/FirstPage/Dashboard';
 function App() {
   return (
     <div className="App">
-      <Testpage></Testpage>
+      {/* <Testpage></Testpage> */}
+      <Dashboard></Dashboard>
     </div>
   );
 }
